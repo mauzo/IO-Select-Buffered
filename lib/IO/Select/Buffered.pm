feda50580836cc5e197c530d145302ff79a1953f
Ben Morrow <ben@morrow.me.uk>
2012-11-23 21:11:21 +0000

    Implement IO::Select::Buffered.
    ->can_read returns early if any of the filehandles have buffered read
    data. ->can_flush is a new method which returns any of the filehandles
    which have buffered write data *and* are selectable for writing.
    
    It would be nice to make ->can_write return early if there is space left
    in the buffer, unfortunately PerlIO doesn't expose this information.

--- lib/IO/Select/Buffered.pm
+++ lib/IO/Select/Buffered.pm
@@ -6,4 +6,37 @@ use strict;
 
 our $VERSION = "1";
 
+use parent "IO::Select";
+
+use Carp;
+use Scalar::Util    qw/openhandle/;
+use IO::Pending     qw/pending_read pending_write/;
+
+sub add {
+    my ($vec, @fhs) = @_;
+    if (my @bad = grep !openhandle($_), @fhs) {
+        Carp::croak sprintf "Bad filehandle%s: %s",
+            (@bad > 1 ? "s" : ""), join ", ", @bad;
+    }
+    $vec->SUPER::add(@fhs);
+}
+
+sub can_read {
+    my ($vec, $timeout) = @_;
+
+    if (my @h = grep pending_read($_), $vec->handles) {
+        return @h;
+    }
+    $vec->SUPER::can_read($timeout);
+}
+
+sub can_flush {
+    my ($vec, $timeout) = @_;
+
+    my @h = grep pending_write($_), $vec->handles
+        or return;
+    my $sel = IO::Select->new(@h);
+    $sel->can_write($timeout);
+}
+
 1;
